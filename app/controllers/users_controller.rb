f5474afa1666fb94090b2f71e045ebb8207652be
Glen Angelo Palacol <glen.palacol@gmail.com>
2012-07-26 16:53:50 +0800

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create 
     @user = User.new(params[:user]) 
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-    else 
-      render 'new' 
-    end 
-  end 
+    else
+      render 'new'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   #get "static_pages/home"
   root to: 'static_pages#home'
   
   match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   #get "static_pages/help"
   match '/help', to: 'static_pages#help'
